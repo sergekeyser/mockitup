b24785420698831e4dc230358ae1ad68c8e9b7a5
Serge Keyser <serge@keyser.com>
2016-05-31 19:10:17 +0200

    initial project
    Author:    Serge Keyser <serge@keyser.com>

---
+++ moc-server.js
@@ -0,0 +1,38 @@
+//Lets require/import the HTTP module
+var http = require('http');
+//
+//Lets define a port we want to listen to
+const PORT=8080; 
+const LocalPath=__dirname+'/mockfiles';
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+	
+	try{
+			var fs = require('fs')
+			var path = require('path')
+			var myfilepath = path.resolve(LocalPath+request.url+".json")
+ 			if(myfilepath.indexOf(LocalPath) != 0)
+				throw new Error('Someone tried to snoop outside of the file dir!')
+			else
+				{
+				var obj = JSON.parse(fs.readFileSync(myfilepath, 'utf8'))
+				response.end(JSON.stringify(obj))
+				}
+		}
+	catch(err)
+		{
+		//send error message
+		console.log('[ERROR]')
+		console.log('While trying to read: '+ myfilepath)
+		console.log('Getting the following error: ')
+		console.log(err)
+		response.end('{ "error": \n { "code":"404",\n "message":"File Not Found"} \n}')					}
+	}
+		//Create a server
+		var server = http.createServer(handleRequest);
+		//Lets start our server
+		 server.listen(PORT, function(){
+		//Callback triggered when server is successfully listening. Hurray!
+		 console.log("Server listening on: http://localhost:%s", PORT);
+		 });
