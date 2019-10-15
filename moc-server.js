f772a5449c68e406813ea7bfc1d8800510cf9ebd
sergekeyser <sergekeyser+gh@gmail.com>
2019-10-15 20:00:18 +0100

    Update moc-server.js

--- moc-server.js
+++ moc-server.js
@@ -2,7 +2,7 @@
 var http = require('http');
 //
 //Lets define a port we want to listen to
-const PORT=8080; 
+const PORT=8081; 
 const LocalPath=__dirname+'/mockfiles';
 
 //We need a function which handles requests and send response
